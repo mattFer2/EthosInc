aa00ed45517e3e9abe738ae4ddd118e139faaa48
n01490036 <n01490036@CISL-2203-73742>
2022-10-27 10:49:39 -0400

    Updated navbar to have Login on the right side, and added some elements

--- EthosInc/Controllers/HomeController.cs
+++ EthosInc/Controllers/HomeController.cs
@@ -34,6 +34,16 @@ namespace EthosInc.Controllers
             return View();
         }
 
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
