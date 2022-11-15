9095ebc72f6cfd3a0d0f5387f060fbdc3b228383
CJ Williams <CharlesWWilliams@hotmail.com>
2022-11-15 14:15:48 -0500

    Add product images to shop page

--- EthosInc/Models/Product.cs
+++ EthosInc/Models/Product.cs
@@ -12,6 +12,8 @@ namespace EthosInc.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
+
+        public string PictureFolder { get; set; }
     }
 
 }
