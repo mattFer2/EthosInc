dd3813fbe17f1d5c6279abbe498dbf35b664ca02
LightningWizard <lightningwizard2001@gmail.com>
2022-11-22 00:18:54 -0500

    Added more questions to the FAQ page as well as re-added the Pictures variable to the Product file, since errors were being thrown without it.

--- EthosInc/Models/Product.cs
+++ EthosInc/Models/Product.cs
@@ -12,6 +12,7 @@ namespace EthosInc.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
+        public string Pictures { get; set; }
     }
 
 }
